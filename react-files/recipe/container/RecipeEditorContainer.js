06ce88816cff7ea3c221bb7e5d1459268a51064e
Your Name <ian.sudderth@gmail.com>
2017-09-05 19:29:37 -0400

    implements new_section actions and controls

--- react-files/recipe/container/RecipeEditorContainer.js
+++ react-files/recipe/container/RecipeEditorContainer.js
@@ -3,14 +3,23 @@ import PropTypes from 'prop-types';
 import Card from 'material-ui/Card';
 import { withStyles } from 'material-ui/styles';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Button from 'material-ui/Button';
+import Add from 'material-ui-icons/Add';
+import { newSection } from '../actions';
 import EditHeader from '../components/edit_mode/EditHeader';
-import EditSection from '../components/edit_mode/EditSection'
+import EditSection from '../components/edit_mode/EditSection';
 
 const styles = {
-	container:{
-		margin:12
-	}
-}
+	container: {
+		margin: 12,
+	},
+	buttonContainer: {
+		display: 'flex',
+		justifyContent: 'center',
+		padding: 12,
+	},
+};
 
 class RecipeEditorContainer extends Component {
 	constructor(props) {
@@ -20,25 +29,35 @@ class RecipeEditorContainer extends Component {
 	}
 
 	render() {
-		const {container} = this.props.classes
+		const { container, buttonContainer } = this.props.classes;
 		return (
 			<div className={container}>
 				<Card>
-				<EditHeader
-					title={this.props.recipe.title}
-					servingAmount={this.props.recipe.serving.amount}
-					servingUnit={this.props.recipe.serving.unit}
-				/>
-				{this.props.recipe.recipe.map( (section,index) => {
-					return(
-					<EditSection
-					key={`recipe-section-${index}`}
-					sectionIndex={index}
-					ingredientsArray={section.ingredients}
-					steps={section.procedure}
+					<EditHeader
+						title={this.props.recipe.title}
+						servingAmount={this.props.recipe.serving.amount}
+						servingUnit={this.props.recipe.serving.unit}
 					/>
-					)
-				})}
+					{this.props.recipe.recipe.map((section, index) => {
+						return (
+							<EditSection
+								key={`recipe-section-${index}`}
+								sectionIndex={index}
+								ingredientsArray={section.ingredients}
+								steps={section.procedure}
+							/>
+						);
+					})}
+					<div className={buttonContainer}>
+						<Button
+							raised
+							color="primary"
+							onClick={this.props.newSection}
+						>
+							<Add />
+							{'New Section'}
+						</Button>
+					</div>
 				</Card>
 			</div>
 		);
@@ -54,9 +73,9 @@ function mapStateToProps({ recipe }) {
 }
 
 function mapDispatchToProps(dispatch) {
-	return {};
+	return bindActionCreators({ newSection }, dispatch);
 }
 
-export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(
-	RecipeEditorContainer,
-));
+export default withStyles(styles)(
+	connect(mapStateToProps, mapDispatchToProps)(RecipeEditorContainer),
+);
--- react-files/recipe/reducers/editModeReducers/editSection.js
+++ react-files/recipe/reducers/editModeReducers/editSection.js
@@ -1,3 +1,20 @@
+import _ from 'lodash';
+import { NEW_SECTION, DELETE_SECTION, REORDER_SECTION } from '../../actions';
+
 export default function editSectionReducer(state, action) {
-	return state;
+	switch (action.type) {
+		case NEW_SECTION: {
+			let newState = _.merge({}, state);
+			let newSection = {
+				ingredients: [
+					{ name: 'Ingredient Name', amount: 1, unit: 'g' },
+				],
+				procedure: ['Do something with something'],
+			};
+			newState.recipe = [...newState.recipe, newSection];
+			return newState;
+		}
+		default:
+			return state;
+	}
 }
